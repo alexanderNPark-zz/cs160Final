66ee20dac2d42b46ebdba5b5f1f5433f0ad58db6
Alexander Park <alexander.n.park@berkeley.edu>
2019-08-06 00:17:36 -0700

    NOTHER

--- main/main.go
+++ main/main.go
@@ -19,54 +19,9 @@ var data string = "255712391091861932312181243211672021836999"
 var location string
 
 
-func indexHandler(w http.ResponseWriter, r *http.Request, ){
-	fmt.Fprintln(w, "<!DOCTYPE html>")
-	fmt.Fprintln(w, "<html>")
-	fmt.Fprintln(w, "<body>")
-	fmt.Fprintln(w, "<input type=\"password\" name =\"pass\" </input>")
-	fmt.Fprintln(w, "<a href =\"initAuth\"> Authorize </a>")
-	fmt.Fprintln(w, "</body>")
-	fmt.Fprintln(w, "</html>")
-}
+
 
 func author(w http.ResponseWriter, r *http.Request, ){
-	if(active){
-		fmt.Fprintln(w, "<!DOCTYPE html>")
-		fmt.Fprintln(w, "<html>")
-		fmt.Fprintln(w, "<body>")
-		fmt.Fprintln(w, "Already Authorized")
-		fmt.Fprintln(w, "<a href =\"invoke\"> Invoke </a>")
-		fmt.Fprintln(w, "</body>")
-		fmt.Fprintln(w, "</html>")
-		return
-	}
-
-	r.ParseForm()
-
-	combine:=""
-	length:=16
-	i:=0
-
-	for i!=length{
-		combine+=r.FormValue("h"+strconv.Itoa(i)+"h")
-		i++
-	}
-
-	if(combine==data){
-		fmt.Fprintf(w, "Authorized")
-		active = true
-
-	}
-	browPass :=r.FormValue("pass")
-	if(!active){
-		if(browPass=="JibunWoo"){
-			fmt.Fprintf(w, "Authorized")
-			active = true
-
-		} else{
-			fmt.Fprintf(w, "Denied")
-		}
-	}
 
 
 }
@@ -75,9 +30,9 @@ func author(w http.ResponseWriter, r *http.Request, ){
 
 
 
-func true_index(w http.ResponseWriter, r *http.Request, ){
+func index(w http.ResponseWriter, r *http.Request, ){
 	//private_tmpl_files := []string{"templates/index.html"}
-	t, err := template.ParseFiles("main/templates/index.html")
+	t, err := template.ParseFiles("templates/index.html")
 	if(err!=nil){
 		fmt.Println(err);
 		return
@@ -101,7 +56,7 @@ func Initialize(){
 
 
 
-	handleMultiplex.HandleFunc("/", true_index)
+	handleMultiplex.HandleFunc("/", index)
 	server:=http.Server{Addr:"0.0.0.0:"+strconv.Itoa(port), Handler:handleMultiplex,}
 	server.ListenAndServe()
 }
