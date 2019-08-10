c77459ca4e1cc19c399e9d3d2ed32d5a0a3382dc
Alexander Park <alexander.n.park@berkeley.edu>
2019-08-10 14:58:49 -0700

    improvement in progress

--- main/authorial.go
+++ main/authorial.go
@@ -22,8 +22,9 @@ type Project struct{
 var Architects = make(map[string]*Architect)
 
 func LoadArchitects(){
-	Architects["Ted Mosby"] = &Architect{Name:"Ted Mosby", Stars:3, Projects:make(map[string]*Project), ImgPath:"/assets/imgs/ted_mosby.jpg.png"}
+	Architects["Ted Mosby"] = &Architect{Name:"Ted Mosby", Stars:3, Projects:make(map[string]*Project), ImgPath:"/assets/imgs/ted_mosby.jpg"}
 	Architects["Bob the Builder"] = &Architect{Name:"Bob the Builder", Stars:3, Projects:make(map[string]*Project),ImgPath:"/assets/imgs/bob_builder.png"}
+	Architects["Bob Parr"] = &Architect{Name:"Bob Par", Stars:3, Projects:make(map[string]*Project),ImgPath:"/assets/imgs/Bob_Parr.jpg"}
 	Architects["Jar-Jar"] = &Architect{Name:"Jar-Jar",Stars:1, Projects:make(map[string]*Project), ImgPath:"/assets/imgs/profile_jar_jar.jpg"}
 }
 
