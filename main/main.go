5b43c7c45cb8beacdca82dff0687cd69494c3d64
Alexander Park <alexander.n.park@berkeley.edu>
2019-08-10 06:37:44 -0700

    improvement in progress

--- main/main.go
+++ main/main.go
@@ -86,7 +86,7 @@ func index(w http.ResponseWriter, r *http.Request){
 func notFound(w http.ResponseWriter, r *http.Request, status int){
 	w.WriteHeader(status)
 	if status == http.StatusNotFound {
-		fmt.Fprint(w, "custom 404")
+		fmt.Fprint(w, "404 Not Found")
 	}
 }
 
