db5720916be2ffcbdf7c5a634fe1446b31093a73
Alexander Park <alexander.n.park@berkeley.edu>
2019-08-11 15:15:52 -0700

    improvement in progress

--- main/webserver.go
+++ main/webserver.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"net/http"
+	"fmt"
+
 )
 
 var upgrader = websocket.Upgrader{
@@ -18,72 +21,58 @@ var unifyingRead = make(chan string)
 //test variable-delete later
 var activated bool = false;
 
-/*
+
 func StartWebserver(w http.ResponseWriter, r *http.Request, ){
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true } //allow all hosts
 	connection,err:=upgrader.Upgrade(w,r,nil)
+
 	if(err!=nil){
 		fmt.Println("Failed to make connection to server")
 		fmt.Println(err)
 		return
 	}
-	address:=strings.Split(r.Header.Get("X-Forwarded-For"),",")
 
-	fmt.Println("Contact")
-    go newClient.read()
-    go newClient.write()
+
+	connection.ReadMessage()
+
+
 
 }
 
-func (client *Client) read(){
-	defer client.connection.Close()
+func ChatRead(connection *websocket.Conn){
+	defer connection.Close()
 
 	for{
-		fmt.Println("Reading Message")
-		_, message, err := client.connection.ReadMessage()
-		fmt.Println("read the message")
+
+		_, message, err := connection.ReadMessage()
+
 		if(err!=nil){
 			fmt.Println("Failure in reading")
 			return
 		}
 
-		client.channel<-string(message)
-		if(!activated){
-			activated = !activated
-			testHandleGenerator()
-		}
+
+
 		fmt.Println(string(message))
 	}
 
 }
 
-func (client *Client) write(){
-	defer client.connection.Close()
+func CharWrite(connection *websocket.Conn){
+	defer connection.Close()
 
 	for{
-		writer, err := client.connection.NextWriter(websocket.TextMessage)
+		writer, err := connection.NextWriter(websocket.TextMessage)
 		if(err!=nil){
 			fmt.Println("Failure in writing")
 			return
 		}
-		sendBack:="Received"+<-client.channel
-		writer.Write([]byte(sendBack))
+
+		//writer.Write([]byte(sendBack))
 	}
 }
 
-func testHandleGenerator(){
-	beta:=func(w http.ResponseWriter, r *http.Request){
-		t, err := template.ParseFiles("templates/workspace.html")
-		if(err!=nil){
 
-			fmt.Println(err);
-			return
-		}
-		t.Execute(w,""); //change when template is generated
-	}
-	Multiplex.HandleFunc("/workspace",beta)
-}
-*/
 
 
 
